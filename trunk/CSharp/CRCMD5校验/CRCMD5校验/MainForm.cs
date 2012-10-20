3586a798dac1fb608d18cf2e3f7e73ee6dfd34ad
zhaotong315 <zhaotong315@fe32744b-2ddc-22c2-02bb-281a093782ec>
2012-10-20 06:23:06 +0000

    增加学习C#Windows编程的项目CRCMD5校验工程。内有模板。

---
+++ trunk/CSharp/CRCMD5校验/CRCMD5校验/MainForm.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRCMD5校验
+{
+    public partial class MainForm : Form
+    {
+        private String filePath;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void onProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+
+        }
+
+        private void onWorkStarted(object sender, DoWorkEventArgs e)
+        {
+
+        }
+
+        private void onWorkCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+
+        }
+
+        private void onDialogConfirm(object sender, CancelEventArgs e)
+        {
+            if (openFileDialog.Equals(sender))
+            {
+                filePath = ((OpenFileDialog)sender).FileName;
+                inputRect.Text = filePath;
+            }
+        }
+
+        private void onClick(object sender, EventArgs e)
+        {
+            openFileDialog.ShowDialog();
+        }
+
+        private void dragDrop(object sender, DragEventArgs e)
+        {
+            foreach (String str in (String[])e.Data.GetData(DataFormats.FileDrop)) {
+                MessageBox.Show(str);
+            }
+        }
+
+        private void dragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) {
+                e.Effect = DragDropEffects.Link;
+            } else {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+    }
+}
