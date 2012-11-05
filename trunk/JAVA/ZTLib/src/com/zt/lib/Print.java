c590e8ab997d5e0ed13d8f62f4d563774103a820
zhaotong315 <zhaotong315@fe32744b-2ddc-22c2-02bb-281a093782ec>
2012-11-05 10:07:16 +0000

    加入自己的工具库

---
+++ trunk/JAVA/ZTLib/src/com/zt/lib/Print.java
@@ -0,0 +1,58 @@
+package com.zt.lib;
+
+import android.database.Cursor;
+import android.util.Log;
+
+public class Print {
+
+	private static boolean printable = true;
+	private static String TAG = "";
+
+	public static void setTAG(String tag)
+	{
+		TAG = tag;
+	}
+	
+	public static void enable()
+	{
+		printable = true;
+	}
+	
+	public static void disable()
+	{
+		printable = false;
+	}
+
+	public static void d(String msg)
+	{
+		if (printable) {
+			StackTraceElement line = new Throwable().getStackTrace()[1];
+
+			if (line != null) {
+				Log.d(TAG, "File[" + line.getFileName() + "]Line[" + line.getLineNumber()
+						+ "]Msg[" + msg + "]");
+			}
+		}
+	}
+
+	public static void w(String msg)
+	{
+		if (printable) {
+			StackTraceElement line = new Throwable().getStackTrace()[1];
+			if (line != null) {
+				Log.d(TAG, "File[" + line.getFileName() + "]Line[" + line.getLineNumber()
+						+ "]Msg[" + msg + "]");
+			}
+		}
+	}
+	
+	public static void cursor(Cursor c)
+	{
+		if (printable) {
+			String names[] = c.getColumnNames();
+			for (String name : names) {
+				d(name + " = " + c.getString(c.getColumnIndex(name)));
+			}
+		}
+	}
+}
