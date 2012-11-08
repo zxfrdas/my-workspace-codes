9e45c5bd8b0e579df2966e2eb3e38958e42bfed3
zhaotong315 <zhaotong315@fe32744b-2ddc-22c2-02bb-281a093782ec>
2012-11-08 16:53:08 +0000

    加入StreamHelperTest.java

---
+++ JAVA/ZTLibImplTester/src/com/zt/lib/impl/test/StreamHelperTest.java
@@ -0,0 +1,44 @@
+package com.zt.lib.impl.test;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import com.zt.lib.Print;
+import com.zt.lib.StreamHelper;
+
+import android.content.Context;
+import android.test.AndroidTestCase;
+
+public class StreamHelperTest extends AndroidTestCase {
+	
+	@Override
+	protected void setUp() throws Exception
+	{
+		super.setUp();
+		Print.setTAG("ZTLibTest");
+		Print.enable();
+		FileOutputStream fos = getContext().openFileOutput("StreamHelper", Context.MODE_PRIVATE);
+		fos.write(new StringBuilder("StreamHelper").toString().getBytes());
+		fos.flush();
+		fos.close();
+	}
+
+	@Override
+	protected void tearDown() throws Exception
+	{
+		super.tearDown();
+	}
+
+	public void testToString()
+	{
+		try {
+			assertEquals("StreamHelper", StreamHelper.toString(getContext().openFileInput("StreamHelper")));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
