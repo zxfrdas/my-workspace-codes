d709b96c66ab580a5d5bb87d1f3c9bf2fa9dc054
zhaotong315 <zhaotong315@fe32744b-2ddc-22c2-02bb-281a093782ec>
2012-11-13 13:19:30 +0000

    增加javadoc

--- trunk/JAVA/ZTLib/src/com/zt/lib/collect/SingletonValueMap.java
+++ trunk/JAVA/ZTLib/src/com/zt/lib/collect/SingletonValueMap.java
@@ -3,6 +3,14 @@ package com.zt.lib.collect;
 import java.util.Hashtable;
 import java.util.Map;
 
+/**
+ * 继承于Hashtable，实现了一个value无法重复的map，增加了一个根据value获取key的方法。
+ * 如果输入的value已经存在于键值对中，并且键与已经存在的不同，那么会将此value覆盖掉已经存在的键值对中的值。
+ * @author zhaotong
+ *
+ * @param <K> 键
+ * @param <V> 值
+ */
 public class SingletonValueMap<K, V> extends Hashtable<K, V> {
 
 	private static final long serialVersionUID = -6033585881328750826L;
@@ -18,6 +26,11 @@ public class SingletonValueMap<K, V> extends Hashtable<K, V> {
 		return super.put(key, value);
 	}
 	
+	/**
+	 * 根据值获取键。因为此Map键值是唯一对应关系故可以实现此功能。
+	 * @param value 
+	 * @return the key linked to the value
+	 */
 	public synchronized K getKeyByValue(Object value)
 	{
 		for (Map.Entry<K, V> entry : entrySet()) {
