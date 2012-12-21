e624b3fef99d97ae96f2006116e30d6397cae4f7
zhaotong315 <zhaotong315@fe32744b-2ddc-22c2-02bb-281a093782ec>
2012-12-21 07:40:54 +0000

    已完成数据显示功能。

--- CSharp/SQLiteEditor/SQLiteEditor/Form1.Designer.cs
+++ CSharp/SQLiteEditor/SQLiteEditor/Form1.Designer.cs
@@ -1,5 +1,5 @@
 ﻿namespace SQLiteEditor {
-    partial class Form1 {
+    partial class MainForm {
         /// <summary>
         /// 必需的设计器变量。
         /// </summary>
@@ -25,31 +25,68 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.treeView1 = new System.Windows.Forms.TreeView();
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("节点0");
+            this.mTreeView = new System.Windows.Forms.TreeView();
+            this.mDataView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.mDataView)).BeginInit();
             this.SuspendLayout();
             // 
-            // treeView1
+            // mTreeView
             // 
-            this.treeView1.Location = new System.Drawing.Point(12, 57);
-            this.treeView1.Name = "treeView1";
-            this.treeView1.Size = new System.Drawing.Size(114, 186);
-            this.treeView1.TabIndex = 0;
+            this.mTreeView.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.mTreeView.Location = new System.Drawing.Point(12, 57);
+            this.mTreeView.Name = "mTreeView";
+            treeNode1.Name = "mBaseNode";
+            treeNode1.Text = "节点0";
+            this.mTreeView.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode1});
+            this.mTreeView.Size = new System.Drawing.Size(164, 263);
+            this.mTreeView.TabIndex = 0;
+            this.mTreeView.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.mTreeView_AfterSelect);
             // 
-            // Form1
+            // mDataView
+            // 
+            this.mDataView.AllowUserToAddRows = false;
+            this.mDataView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.mDataView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.mDataView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.mDataView.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnEnter;
+            this.mDataView.Location = new System.Drawing.Point(191, 57);
+            this.mDataView.MultiSelect = false;
+            this.mDataView.Name = "mDataView";
+            this.mDataView.ReadOnly = true;
+            this.mDataView.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
+            this.mDataView.RowTemplate.Height = 23;
+            this.mDataView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.mDataView.Size = new System.Drawing.Size(593, 263);
+            this.mDataView.TabIndex = 1;
+            this.mDataView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataView_CellContentClick);
+            this.mDataView.RowsAdded += new System.Windows.Forms.DataGridViewRowsAddedEventHandler(this.onRowAdded);
+            this.mDataView.RowsRemoved += new System.Windows.Forms.DataGridViewRowsRemovedEventHandler(this.onRowRemoved);
+            // 
+            // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(676, 321);
-            this.Controls.Add(this.treeView1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(816, 401);
+            this.Controls.Add(this.mDataView);
+            this.Controls.Add(this.mTreeView);
+            this.Name = "MainForm";
+            this.Text = "SQLiteEditor";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.onFormClose);
+            this.Load += new System.EventHandler(this.onFormLoad);
+            ((System.ComponentModel.ISupportInitialize)(this.mDataView)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.TreeView mTreeView;
+        private System.Windows.Forms.DataGridView mDataView;
     }
 }
 
--- CSharp/SQLiteEditor/SQLiteEditor/Form1.cs
+++ CSharp/SQLiteEditor/SQLiteEditor/Form1.cs
@@ -9,21 +9,85 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SQLite;
 using System.Data.Common;
+using System.Collections;
 
 namespace SQLiteEditor {
-    public partial class Form1 : Form {
-        public Form1()
+    public partial class MainForm : Form {
+
+        private SQLiteConnection sqlCon = null;
+        private DbDataReader sqlReader = null;
+
+        public MainForm()
         {
             InitializeComponent();
-            SQLiteConnection conn = (SQLiteConnection)new SQLiteFactory().CreateConnection();
-            conn.ConnectionString = @"Data Source=./factory.db";
-            conn.Open();
-            DbCommand sqlCommand = new SQLiteCommand("SELECT tbl_name FROM sqlite_master" +
-                " WHERE type='table' order by name;", conn);
-            DbDataReader sqlReader = sqlCommand.ExecuteReader();
+            
+        }
+
+        private void onFormLoad(object sender, EventArgs e)
+        {
+            sqlCon = DataBaseHelper.connectDB("./factory.db");
+            sqlCon.Open();
+            sqlReader = DataBaseHelper.queryReturnReader(sqlCon, "tbl_name",
+                "sqlite_master", "type='table'");
+            TreeNode[] tbleNameNodes = new TreeNode[DataBaseHelper.getAllTableName(sqlCon).Length];
+            int index = 0;
             while (sqlReader.Read()) {
-                Console.WriteLine("table name = " + sqlReader.GetName(0));
+                tbleNameNodes[index] = new TreeNode(sqlReader.GetString(
+                    sqlReader.GetOrdinal("tbl_name")));
+                index++;
+            }
+            mTreeView.TopNode.Text = "factory.db";
+            mTreeView.TopNode.Nodes.AddRange(tbleNameNodes);
+        }
+
+        private void onFormClose(object sender, FormClosedEventArgs e)
+        {
+            if (null != sqlReader) {
+                sqlReader.Close();
+                sqlReader.Dispose();
             }
         }
+
+        private void mTreeView_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            bool bIsTableNode = false;
+            if (0 != e.Node.Level) {
+                bIsTableNode = true;
+            }
+            if (bIsTableNode) {
+                mDataView.DataSource = DataBaseHelper.queryReturnTable(sqlCon, "*", e.Node.Text, null);
+            }
+        }
+
+        private void dataView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void onRowAdded(object sender, DataGridViewRowsAddedEventArgs e)
+        {
+            updateRowHeader(e.RowIndex, e.RowCount);
+        }
+
+        private void onRowRemoved(object sender, DataGridViewRowsRemovedEventArgs e)
+        {
+            updateRowHeader(e.RowIndex, e.RowCount);
+        }
+
+        private void updateRowHeader(int rowIndex, int rowCount)
+        {
+            if (0 == mDataView.RowCount) {
+                return;
+            }
+            for (int i = 0; i < rowCount; i++) {
+                mDataView.Rows[rowIndex + i].HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleRight;
+                mDataView.Rows[rowIndex + i].HeaderCell.Value = (rowIndex + i + 1).ToString();
+            }
+            for (int i = rowIndex + rowCount; i < mDataView.Rows.Count; i++) {
+                mDataView.Rows[i].HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleRight;
+                mDataView.Rows[i].HeaderCell.Value = (i + 1).ToString();
+            }
+        }
+
     }
 }
