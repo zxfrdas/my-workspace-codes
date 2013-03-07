03fc9a21c3aab4e3137b505b7e864a2010faa11d
zhaotong315 <zhaotong315@fe32744b-2ddc-22c2-02bb-281a093782ec>
2013-03-07 14:09:27 +0000

    淘宝获取SessionKey工程

---
+++ trunk/CSharp/GetSessionkey/GetSessionkey/FrmGetSessionKey.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GetSessionkey
+{
+    public partial class FrmGetSessionKey : Form
+    {
+        /// <summary>
+        /// 授权码
+        /// </summary>
+        public string AuthrizeCode = "";
+        private string url = "http://open.taobao.com/authorize/?appkey=21180870";
+
+        public FrmGetSessionKey()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 获取HTML页面内制定Key的Value内容
+        /// </summary>
+        /// <param name="html"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public string GetHiddenKeyValue(string html, string key)
+        {
+            string str = html.Substring(html.IndexOf(key));
+            str = str.Substring(str.IndexOf("value") + 7);
+            int eindex1 = str.IndexOf("'");
+            int eindex2 = str.IndexOf("\"");
+            int eindex = eindex2;
+            if (eindex1 >= 0 && eindex1 < eindex2)
+            {
+                eindex = eindex1;
+            }
+            return str.Substring(0, eindex);
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            if (e.Url.AbsoluteUri == url)
+            {
+                AuthrizeCode = GetHiddenKeyValue(this.wbGetSessionkey.DocumentText, "autoInput");
+                if (!string.IsNullOrEmpty(AuthrizeCode) && AuthrizeCode.IndexOf("TOP-") >= 0)
+                {
+                    this.DialogResult = DialogResult.OK;
+                    this.Close();
+                }
+            }
+        }
+
+        private void FrmGetSessionKey_Load(object sender, EventArgs e)
+        {
+            wbGetSessionkey.Navigate(url);
+        }
+
+    }
+}
