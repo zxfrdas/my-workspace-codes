3f66345c429fc32d50f333a575d18b66e0493d8a
zhaotong315 <zhaotong315@fe32744b-2ddc-22c2-02bb-281a093782ec>
2013-03-25 13:20:35 +0000

    正在加入解析“手机充电宝”的逻辑。为之后解析任意商品做基础

--- trunk/JAVA/TaoBaoDataAnalyst/src/com/zt/taobao/Main.java
+++ trunk/JAVA/TaoBaoDataAnalyst/src/com/zt/taobao/Main.java
@@ -1,26 +1,29 @@
 package com.zt.taobao;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-	private static final String RESULT = "http://s.taobao.com/search?spm=a230r.1.8.7.aElfgy&q=" +
-			"hdmi%C1%AC%BD%D3%CF%DF&initiative_id=staobaoz_20130323&sort=sale-desc&atype=b&" +
-			"filterFineness=2&bcoffset=1&newpre=null&style=list&s=0#J_Filter";
-	
-	private static final int DEEPTH = 4;
+	private static final int DEEPTH = 1;
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		List<Item> items = new ArrayList<Item>();
-		HttpParser parser = new HttpParser();
+		HDMIParser parser = new HDMIParser();
 		HttpVisitor visitor = new HttpVisitor();
 		String nextPage = null;
 		String content = null;
 		for (int i = 0; i < DEEPTH; i ++) {
-			content = visitor.getHtml(RESULT);
+			try {
+				content = visitor.getHtml(String.format(Params.SEARCH,
+						URLEncoder.encode("HDMI连接线", "UTF-8")));
+			} catch (UnsupportedEncodingException e) {
+				e.printStackTrace();
+			}
 			parser.parserShopPage(visitor.getHtml(parser
 					.parserResultPage(content, i)));
 			content = null;
