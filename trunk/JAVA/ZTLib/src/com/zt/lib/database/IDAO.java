5a0ddf51be9d0bcca0ad7a21c77021ace5dc0048
zhaotong315 <zhaotong315@fe32744b-2ddc-22c2-02bb-281a093782ec>
2014-05-06 16:18:47 +0000

    更新库结构，跟信数据库部分

--- trunk/JAVA/ZTLib/src/com/zt/lib/database/IDAO.java
+++ trunk/JAVA/ZTLib/src/com/zt/lib/database/IDAO.java
@@ -2,92 +2,85 @@ package com.zt.lib.database;
 
 import java.util.List;
 
-/**
- * 数据库操作接口
- * @author zhaotong
- * @param <T> 数据库对应JAVA层数据类
- * @param <K> 操作依据
- */
-public interface IDAO<T, K> {
+public interface IDAO<T> {
 
 	/**
 	 * 数据库单个插入操作。
-	 * @param table 进行插入操作的表名
 	 * @param item 试图插入的数据类
 	 * @return 成功返回{@code true}，反之{@code false}
 	 */
-	public boolean insert(String table, T item);
-	
-	/**
-	 * 数据库单个删除操作。
-	 * @param table 进行删除操作的表名
-	 * @param key 删除依据
-	 * @return 成功返回{@code true}，反之{@code false}
-	 */
-	public boolean delete(String table, K key);
-	
-	/**
-	 * 数据库单个更新操作
-	 * @param table 进行更新操作的表名
-	 * @param item 试图更新的数据类
-	 * @return 成功返回{@code true}，反之{@code false}
-	 */
-	public boolean update(String table, T item);
-	
-	/**
-	 * 数据库单个查询操作
-	 * @param table 进行查询操作的表名
-	 * @param key 查询依据
-	 * @return 查询所得数据类
-	 */
-	public T query(String table, K key);
+	public boolean insert(T item);
 	
 	/**
 	 * 数据库多条插入操作。
-	 * @param table 进行插入操作的表名
 	 * @param items 试图插入的数据类集合
 	 * @return 成功返回{@code true}，反之{@code false}
 	 */
-	public boolean insertList(String table, List<T> items);
-
+	public boolean insert(List<T> items);
+	
 	/**
-	 * 数据库多条删除操作
-	 * @param table 进行删除操作的表名
-	 * @param items 试图删除的数据类集合
+	 * 数据库单个删除操作。
+	 * @param condition 操作的条件
 	 * @return 成功返回{@code true}，反之{@code false}
 	 */
-	public boolean deleteList(String table, List<T> items);
+	public boolean delete(ExecCondition condition);
+	
+	/**
+	 * 数据库多条删除操作
+	 * @param items 试图删除的数据类集合
+	 * @param condition 操作的条件
+	 * @return 成功返回{@code true}，反之{@code false}
+	 */
+	public boolean delete(List<T> items, ExecCondition condition);
 	
 	/**
 	 * 数据库表删除所有条目操作
-	 * @param table 进行删除操作的表名
 	 * @return 成功返回{@code true}，反之{@code false}
 	 */
-	public boolean deleteAll(String table);
+	public boolean deleteAll();
+	
+	/**
+	 * 数据库单个更新操作
+	 * @param item 试图更新的数据类
+	 * @param condition 操作的条件
+	 * @return 成功返回{@code true}，反之{@code false}
+	 */
+	public boolean update(T item, ExecCondition condition);
 	
 	/**
 	 * 数据库多条更新操作
-	 * @param table 进行更新操作的表名
 	 * @param items 试图更新的数据类集合
+	 * @param condition 操作的条件
 	 * @return 成功返回{@code true}，反之{@code false}
 	 */
-	public boolean updateList(String table, List<T> items);
+	public boolean updateList(List<T> items, ExecCondition condition);
+	
+	/**
+	 * 数据库全部更新操作
+	 * @param items 试图更新的数据
+	 * @return 成功返回{@code true}，反之{@code false}
+	 */
+	public boolean updateAll(T item);
+	
+	/**
+	 * 数据库单个查询操作
+	 * @param condition 操作的条件
+	 * @return 查询所得数据类
+	 */
+	public T query(ExecCondition condition);
 	
 	/**
 	 * 数据库查询表中所有数据
-	 * @param table 试图进行查询操作的表名
-	 * @param orderBy 结果排序依据
+	 * @param condition 操作的条件
 	 * @return 查询所得数据类集合
 	 */
-	public List<T> queryAll(String table, String orderBy);
+	public List<T> queryAll(ExecCondition condition);
 	
 	/**
-	 * 获取指定表中共有多少行数据
-	 * @param table 试图进行查询的表名
+	 * 获取共有多少行数据
 	 * @return 表的行数
 	 */
-	public int getCount(String table);
-	
+	public int getCount();
 	
 	/**
 	 * 关闭数据库
--- trunk/JAVA/ZTLib/src/com/zt/lib/database/test/TestItem.java
+++ trunk/JAVA/ZTLib/src/com/zt/lib/database/test/TestItem.java
@@ -1,8 +1,22 @@
 package com.zt.lib.database.test;
 
+import com.zt.lib.database.Column;
+import com.zt.lib.database.SQLite3DataType;
 import com.zt.lib.database.Table;
 
 @Table(name="tbl_Test")
 public class TestItem {
+	
+	@Column(index=1, name="column_1", type=SQLite3DataType.INTEGER)
+	public int testInt;
+	
+	@Column(index=2, name="column_2", type=SQLite3DataType.REAL)
+	public double testFloat;
+	
+	@Column(index=3, name="column_3", type=SQLite3DataType.INTEGER)
+	public boolean testBoolean;
+	
+	@Column(index=4, name="column_4", type=SQLite3DataType.TEXT)
+	public String testString;
 	
 }
